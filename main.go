0831a53fdfb75c8d6a092a8d8331954262b0a15a
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2021-08-07 10:13:27 +0700

    learn simple GET Request using handler gin & make simple router using gin.Default() notes: this is not best practice, this is just hands on

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"bwastartup/user"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -35,4 +37,25 @@ func main() {
 	for _, user := range users {
 		fmt.Println("Hello My name is ", user.Name, " and my job is ", user.Occupation)
 	}
+
+	router := gin.Default()
+	// params handler sebelah kanan mengacu pada function handler
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+// handler untuk gin
+func handler(c *gin.Context) {
+	// caranya belum best practice yang ini, harus connect db dulu
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var users []user.User
+	db.Find(&users)
+
+	c.JSON(http.StatusOK, users)
 }
